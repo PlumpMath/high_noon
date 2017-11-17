19215d05ef485aafed1c27af86b5fcbf42351cad
Joseph Conley <joseph.j.conley@gmail.com>
2017-11-17 17:27:06 -0500

    Add more textures, scrolling background, enemy improvement
    Enemy wont file until he is visible

--- Scripts/enemy_area2d.gd
+++ Scripts/enemy_area2d.gd
@@ -4,17 +4,19 @@ const MAX_DAMAGE = 100
 const SHOT_TIMEOUT = 100
 
 var current_damage = 0
+var can_shoot = false
 var current_shot_timer = SHOT_TIMEOUT
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	if (current_shot_timer > 1):
-		current_shot_timer -= (delta * 100)
-	else:
-		global.shoot_bullet(get_node("Position2D").get_global_pos(), -1)
-		current_shot_timer = SHOT_TIMEOUT
+	if (can_shoot):
+		if (current_shot_timer > 1):
+			current_shot_timer -= (delta * 100)
+		else:
+			global.shoot_bullet(get_node("Position2D").get_global_pos(), -1)
+			current_shot_timer = SHOT_TIMEOUT
 
 #check if we have been hit and do damange
 func _on_enemy_area2d_area_enter( area ):
@@ -26,3 +28,8 @@ func _on_enemy_area2d_area_enter( area ):
 
 func _on_enemy_area2d_body_enter( body ):
 	print("Enemy_hit_body_enter")
+
+
+func _on_VisibilityNotifier2D_enter_screen():
+	print("Enemy_enters_screen")
+	can_shoot = true
